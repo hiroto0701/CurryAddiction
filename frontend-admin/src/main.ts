50b9c773e237e087fc6859de24f4f07de8aacc9d
hiroto <hiroto.soccer.0701@gmail.com>
2024-02-14 01:09:41 +0900

    tailwind導入

--- frontend-admin/src/main.ts
+++ frontend-admin/src/main.ts
@@ -1,10 +1,14 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
+
+import './style.css'
+import axios from 'axios'
+
+// api側にリクエスト送る際のURLを設定
+axios.defaults.baseURL = import.meta.env.VITE_APP_API_ENDPOINT;
 
 const app = createApp(App)
 
