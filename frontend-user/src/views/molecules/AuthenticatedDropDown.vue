a2fe1faf3e6de9790c9a5f3e0d64e5bf905a2c88
hiroto <hiroto.soccer.0701@gmail.com>
2024-02-18 23:47:55 +0900

    dashboard整備

---
+++ frontend-user/src/views/molecules/AuthenticatedDropDown.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import { MenuItem } from '@headlessui/vue'
+import { RouterLink } from 'vue-router'
+import LogoutIcon from '@/views/atoms/icons/LogoutIcon.vue'
+import SettingIcon from '@/views/atoms/icons/SettingIcon.vue'
+import MainDropDown from '@/views/atoms/MainDropDown.vue';
+
+const username = 'hiroto';
+</script>
+
+<template>
+  <MainDropDown :username="username">
+    <MenuItem class="flex items-center gap-x-2 text-sm">
+      <router-link :to="{ name: 'Settings' }" class="block px-4 py-2 text-sm text-utility hover:bg-slate-100 hover:text-main">
+        <SettingIcon />設定
+      </router-link>
+    </MenuItem>
+    
+    <MenuItem class="flex items-center gap-x-2 text-sm">
+      <router-link to="#" class="block px-4 py-2 text-sm text-utility hover:bg-slate-100 hover:text-main">
+        <LogoutIcon />ログアウト
+      </router-link>
+    </MenuItem>
+  </MainDropDown>
+</template>
---
+++ frontend-user/src/views/pages/Search.vue
@@ -0,0 +1,5 @@
+<script setup lang="ts">
+</script>
+<template>
+  <h1>検索ページ建設予定地</h1>
+</template>
