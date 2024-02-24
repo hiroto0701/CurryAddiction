83c0ffe1582142e6ba785bc5a23eabd577683fb8
hiroto <hiroto.soccer.0701@gmail.com>
2024-02-24 23:02:49 +0900

    コンポーネント整備

--- frontend-user/src/views/pages/Account/List.vue
+++
@@ -1,5 +0,0 @@
-<script setup lang="ts">
-</script>
-<template>
-  <h1>各ユーザの投稿一覧ページ</h1>
-</template>
--- frontend-user/src/views/pages/Dashboard/Trash.vue
+++
@@ -1,7 +0,0 @@
-<script setup lang="ts">
-</script>
-<template>
-  <header class="flex items-center gap-3 text-xl text-main">
-    <h1>ごみ箱</h1>
-  </header>
-</template>
