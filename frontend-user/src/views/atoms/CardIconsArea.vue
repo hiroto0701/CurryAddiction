09db79bc0566bf7a4d3de4bc6e7aad66e94b12b1
hiroto <hiroto.soccer.0701@gmail.com>
2024-02-28 23:55:51 +0900

    cardコンポーネント修正

---
+++ frontend-user/src/views/atoms/CardIconsArea.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import HeartIcon from '@/views/atoms/icons/HeartIcon.vue'
+import ArchiveIcon from '@/views/atoms/icons/ArchiveIcon.vue'
+</script>
+<template>
+  <div class="flex justify-between h-fit px-3 py-1.5">
+    <HeartIcon class="text-gray-700 cursor-pointer" />
+    <ArchiveIcon class="text-gray-700 cursor-pointer" />
+  </div>
+</template>
---
+++ frontend-user/src/views/molecules/Card.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import CardImg from '@/views/atoms/CardImg.vue'
+import CardBody from '@/views/molecules/card/CardBody.vue'
+</script>
+<template>
+  <div class="h-80 rounded-lg overflow-hidden border border-gray-200">
+    <div class="w-full h-52 cursor-pointer duration-300 hover:opacity-80">
+      <CardImg :src="'https://pbs.twimg.com/media/Fe6MtHRVUAQqqmF.jpg'" />
+    </div>
+
+    <CardBody store-name="和平カレー" location="福岡" date="2024年12月24日" />
+
+  </div>
+</template>
