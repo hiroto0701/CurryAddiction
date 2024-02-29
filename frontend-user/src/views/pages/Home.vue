edc3a03baf62fa097574d09d2137346041834e76
hiroto <hiroto.soccer.0701@gmail.com>
2024-02-29 23:26:14 +0900

    コンポーネント修正

--- frontend-user/src/views/pages/Home.vue
+++ frontend-user/src/views/pages/Home.vue
@@ -1,13 +1,13 @@
 <script setup lang="ts">
-import Card from '@/views/molecules/Card.vue'
+import Card from '@/views/molecules/card/Card.vue'
 </script>
 <template>
   <div class="grid grid-cols-1 gap-x-7 gap-y-12 xssm:grid-cols-2 md:grid-cols-3">
-    <!-- card -->
     <Card :src="'https://pbs.twimg.com/media/Fe6MtHRVUAQqqmF.jpg'" store-name="和平カレー" location="福岡" date="2024年12月24日" />
     <Card :src="'https://livedoor.blogimg.jp/mantaro_club/imgs/e/a/eae0ac07.jpg'" store-name="ダメヤ" location="福岡" date="2024年12月24日" />
     <Card :src="'https://umaga.net/wp-content/uploads/2021/08/DSC02736-1200x798.jpg'" store-name="ヌワラエリヤ" location="福岡" date="2024年12月24日" />
     <Card :src="'https://img.rurubu.jp/img_srw/andmore/images/0000239745/yMJCHzJxBlvH94UqlUlXon7iIQyj6FZonEaXOitI.jpeg'" store-name="クボカリー" location="福岡" date="2024年12月24日" />
     <Card :src="'https://japanesecurry.net/spicy_cms/wp-content/uploads/2019/12/afterglow-520x390.jpg'" store-name="アフターグロウ" location="福岡" date="2024年12月24日" />
+    <Card :src="'https://kbc.co.jp/up/content/15/52/f9d7a4913873066e0ec8713aa5fcb3180add5b38.jpg'" store-name="Rスリランカ" location="福岡" date="2024年12月24日" />
   </div>
 </template>
