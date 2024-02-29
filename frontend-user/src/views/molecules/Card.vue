060d6c0a9fb5689faaa2f6ec94ffec5b0276bd1c
hiroto <hiroto.soccer.0701@gmail.com>
2024-02-29 12:59:20 +0900

    cardコンポーネント細分化。propps使ってるけどディレクトリ構成は考える必要ありかも

--- frontend-user/src/views/molecules/Card.vue
+++ frontend-user/src/views/molecules/Card.vue
@@ -1,14 +1,19 @@
 <script setup lang="ts">
 import CardImg from '@/views/atoms/CardImg.vue'
 import CardBody from '@/views/molecules/card/CardBody.vue'
+
+interface Props {
+  readonly src: string,
+  readonly storeName: string,
+  readonly location: string,
+  readonly date: string,
+}
+
+defineProps<Props>();
 </script>
 <template>
   <div class="h-80 rounded-lg overflow-hidden border border-gray-200">
-    <div class="w-full h-52 cursor-pointer duration-300 hover:opacity-80">
-      <CardImg :src="'https://pbs.twimg.com/media/Fe6MtHRVUAQqqmF.jpg'" />
-    </div>
-
-    <CardBody store-name="和平カレー" location="福岡" date="2024年12月24日" />
-
+    <CardImg :src="src" />
+    <CardBody :store-name="storeName" :location="location" :date="date" />
   </div>
 </template>
