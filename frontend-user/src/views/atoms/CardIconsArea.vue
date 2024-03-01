8b198bd0bb2b39d5126f8536f2cf58eae49f2aa8
hiroto <hiroto.soccer.0701@gmail.com>
2024-03-01 23:23:21 +0900

    クリックイベント追加

--- frontend-user/src/views/atoms/CardIconsArea.vue
+++ frontend-user/src/views/atoms/CardIconsArea.vue
@@ -1,10 +1,14 @@
 <script setup lang="ts">
 import HeartIcon from '@/views/atoms/icons/HeartIcon.vue'
 import ArchiveIcon from '@/views/atoms/icons/ArchiveIcon.vue'
+
+const click = (): void => {
+  console.log('clicked');
+}
 </script>
 <template>
   <div class="flex justify-between h-fit px-3 py-1.5">
-    <HeartIcon class="text-gray-700 cursor-pointer" />
-    <ArchiveIcon class="text-gray-700 cursor-pointer" />
+    <HeartIcon class="text-gray-700 cursor-pointer" @click="click" />
+    <ArchiveIcon class="text-gray-700 cursor-pointer" @click="click" />
   </div>
 </template>
