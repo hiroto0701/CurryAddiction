5e8423e8524e5db9b9bc3f8e5225753ace6f9696
hiroto <hiroto.soccer.0701@gmail.com>
2024-03-04 23:44:59 +0900

    コンポーネント編集

--- frontend-user/src/views/molecules/card/CardBody.vue
+++ frontend-user/src/views/molecules/card/CardBody.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import CardIconsArea from '@/views/atoms/CardIconsArea.vue'
+import CardIcons from '@/views/molecules/card/CardIcons.vue'
 import StoreNameBrowseItem from '@/views/molecules/browseItems/StoreNameBrowseItem.vue'
 import StoreLocationBrowseItem from '@/views/molecules/browseItems/StoreLocationBrowseItem.vue'
 
@@ -13,7 +13,7 @@ defineProps<Props>();
 </script>
 <template>
   <div class="flex flex-col justify-between h-28 pb-4">
-    <CardIconsArea />
+    <CardIcons />
     <StoreNameBrowseItem :store-name="storeName" />
     <StoreLocationBrowseItem :location="location" :date="date" />
   </div>
