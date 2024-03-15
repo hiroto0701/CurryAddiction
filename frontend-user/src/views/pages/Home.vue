d6ba40575f5c3e1581cdee978c9f8b2587b6d0ec
hiroto <hiroto.soccer.0701@gmail.com>
2024-03-15 23:53:41 +0900

    ページトップスクロール機能実装、コンポーネント化

--- frontend-user/src/views/pages/Home.vue
+++ frontend-user/src/views/pages/Home.vue
@@ -10,5 +10,17 @@ import CardDisplayAreaLayout from '@/views/templates/CardDisplayAreaLayout.vue'
     <Card :src="'https://img.rurubu.jp/img_srw/andmore/images/0000239745/yMJCHzJxBlvH94UqlUlXon7iIQyj6FZonEaXOitI.jpeg'" store-name="クボカリー" location="福岡" date="2024年12月24日" />
     <Card :src="'https://japanesecurry.net/spicy_cms/wp-content/uploads/2019/12/afterglow-520x390.jpg'" store-name="アフターグロウ" location="福岡" date="2024年12月24日" />
     <Card :src="'https://kbc.co.jp/up/content/15/52/f9d7a4913873066e0ec8713aa5fcb3180add5b38.jpg'" store-name="Rスリランカ" location="福岡" date="2024年12月24日" />
+    <Card :src="'https://pbs.twimg.com/media/Fe6MtHRVUAQqqmF.jpg'" store-name="和平カレー" location="福岡" date="2024年12月24日" />
+    <Card :src="'https://livedoor.blogimg.jp/mantaro_club/imgs/e/a/eae0ac07.jpg'" store-name="ダメヤ" location="福岡" date="2024年12月24日" />
+    <Card :src="'https://umaga.net/wp-content/uploads/2021/08/DSC02736-1200x798.jpg'" store-name="ヌワラエリヤ" location="福岡" date="2024年12月24日" />
+    <Card :src="'https://img.rurubu.jp/img_srw/andmore/images/0000239745/yMJCHzJxBlvH94UqlUlXon7iIQyj6FZonEaXOitI.jpeg'" store-name="クボカリー" location="福岡" date="2024年12月24日" />
+    <Card :src="'https://japanesecurry.net/spicy_cms/wp-content/uploads/2019/12/afterglow-520x390.jpg'" store-name="アフターグロウ" location="福岡" date="2024年12月24日" />
+    <Card :src="'https://kbc.co.jp/up/content/15/52/f9d7a4913873066e0ec8713aa5fcb3180add5b38.jpg'" store-name="Rスリランカ" location="福岡" date="2024年12月24日" />
+    <Card :src="'https://pbs.twimg.com/media/Fe6MtHRVUAQqqmF.jpg'" store-name="和平カレー" location="福岡" date="2024年12月24日" />
+    <Card :src="'https://livedoor.blogimg.jp/mantaro_club/imgs/e/a/eae0ac07.jpg'" store-name="ダメヤ" location="福岡" date="2024年12月24日" />
+    <Card :src="'https://umaga.net/wp-content/uploads/2021/08/DSC02736-1200x798.jpg'" store-name="ヌワラエリヤ" location="福岡" date="2024年12月24日" />
+    <Card :src="'https://img.rurubu.jp/img_srw/andmore/images/0000239745/yMJCHzJxBlvH94UqlUlXon7iIQyj6FZonEaXOitI.jpeg'" store-name="クボカリー" location="福岡" date="2024年12月24日" />
+    <Card :src="'https://japanesecurry.net/spicy_cms/wp-content/uploads/2019/12/afterglow-520x390.jpg'" store-name="アフターグロウ" location="福岡" date="2024年12月24日" />
+    <Card :src="'https://kbc.co.jp/up/content/15/52/f9d7a4913873066e0ec8713aa5fcb3180add5b38.jpg'" store-name="Rスリランカ" location="福岡" date="2024年12月24日" />
   </CardDisplayAreaLayout>
 </template>
--- frontend-user/src/views/templates/pages/DefaultLayout.vue
+++ frontend-user/src/views/templates/pages/DefaultLayout.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import UserHeader from '@/views/organisms/UserHeader.vue';
-
+import ScrollToTopButton from '@/views/molecules/buttons/ScrollToTopButton.vue'
 </script>
 
 <template>
@@ -8,6 +8,7 @@ import UserHeader from '@/views/organisms/UserHeader.vue';
     <UserHeader />
     <main class="mx-auto my-12 w-full px-6 xs:px-7 sm:px-10 max-w-4xl flex h-auto flex-col">
       <RouterView />
+      <ScrollToTopButton />
     </main>
   </div>
 </template>
