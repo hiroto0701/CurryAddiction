e6285e345de666c1dc6cce141bc531e250e5a404
hiroto <hiroto.soccer.0701@gmail.com>
2024-03-16 23:35:56 +0900

    Dropdown修正

--- frontend-user/src/views/molecules/dropdown/AuthenticatedDropDown.vue
+++ frontend-user/src/views/molecules/dropdown/AuthenticatedDropDown.vue
@@ -6,17 +6,17 @@ import DashboardIcon from '@/views/atoms/icons/DashboardIcon.vue'
 import HeartIcon from '@/views/atoms/icons/HeartIcon.vue'
 import ArchiveIconVue from '@/views/atoms/icons/ArchiveIcon.vue'
 import TrashIconVue from '@/views/atoms/icons/TrashIcon.vue'
-import HeaderDropDown from '@/views/atoms/HeaderDropDown.vue'
+import MainDropDown from '@/views/atoms/MainDropDown.vue'
 
 const username = 'hiroto';
 </script>
 <template>
-   <HeaderDropDown :username>
+   <MainDropDown :username>
     <DropDownMenuItem :to="{ name: 'PostDashboard' }" label="ダッシュボード" :iconComponent="DashboardIcon" />
     <DropDownMenuItem :to="{ name: 'LikedPostDashboard' }" label="いいねした投稿" :iconComponent="HeartIcon" />
     <DropDownMenuItem :to="{ name: 'ArchivedPostDashboard' }" label="保存した投稿" :iconComponent="ArchiveIconVue" />
     <DropDownMenuItem :to="{ name: 'TrashDashboard' }" label="ごみ箱" :iconComponent="TrashIconVue" />
     <DropDownMenuItem :to="{ name: 'Settings' }" label="設定" :iconComponent="SettingIcon" />
     <DropDownMenuItem :to="{ name: 'Home' }" label="ログアウト" :iconComponent="LogoutIcon" />
-  </HeaderDropDown>
+  </MainDropDown>
 </template>
