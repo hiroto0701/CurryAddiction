90690adabb32cb29e7287e30d048c54d32560ce5
hiroto <hiroto.soccer.0701@gmail.com>
2024-03-20 23:23:04 +0900

    ミートボールメニュー、ページトップスクロールボタンに対してツールチップ導入

---
+++ frontend-user/src/views/molecules/tooltips/TopTooltip.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import TooltipContainer from '@/views/atoms/TooltipContainer.vue'
+import TooltipText from '@/views/atoms/TooltipText.vue'
+
+interface Props {
+  readonly text: string
+  readonly open?: boolean
+}
+
+defineProps<Props>();
+</script>
+<template>
+  <TooltipContainer>
+    <slot />
+    <TooltipText :text :open />
+  </TooltipContainer>
+</template>
