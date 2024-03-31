53cc45a032813eba5f7be50e769f371bc30e1b58
hiroto <hiroto.soccer.0701@gmail.com>
2024-03-31 22:56:15 +0900

    前回の修正を復旧

--- laravel/app/Models/User.php
+++ laravel/app/Models/User.php
@@ -27,7 +27,9 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $fillable = [
-       
+        'name',
+        'email',
+        'password',
     ];
 
     /**
