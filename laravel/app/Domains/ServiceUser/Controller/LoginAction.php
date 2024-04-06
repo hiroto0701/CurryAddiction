4ee717322525ddbbaf5720b4b0674061b4f1dca4
hiroto <hiroto.soccer.0701@gmail.com>
2024-04-06 23:31:29 +0900

    loginaction編集

--- laravel/app/Domains/ServiceUser/Controller/LoginAction.php
+++ laravel/app/Domains/ServiceUser/Controller/LoginAction.php
@@ -10,44 +10,41 @@ use App\Models\ManagementCompany;
 use App\Models\Staff;
 use App\Models\User;
 use App\Models\OperationLog;
-use App\Traits\OperationLogTrait;
 use Illuminate\Auth\AuthenticationException;
 use Illuminate\Routing\Controller;
 use Illuminate\Support\Facades\Auth;
 
 class LoginAction extends Controller
 {
-    use OperationLogTrait;
+  public const OPERATION_OVERVIEW = '管理会社社員ログイン';
 
-    public const OPERATION_OVERVIEW = '管理会社社員ログイン';
+  /**
+   * @param LoginRequest $request
+   * @return StaffResource
+   */
+  public function __invoke(LoginRequest $request): StaffResource
+  {
+      // 認証
+      if (!Auth::guard('staffs')->attempt($request->only(['email', 'password']) + [
+          'status' => Staff::STATUS_ENABLED,
+          function($query) {
+              $query->whereHas('company', function($query) {
+                  $query->whereIn('status', [ManagementCompany::STATUS_ENABLED, ManagementCompany::STATUS_SUSPEND]);
+              });
+          },
+      ] + ['token' => $request->token])) {
+          throw new AuthenticationException();
+      }
 
-    /**
-     * @param LoginRequest $request
-     * @return StaffResource
-     */
-    public function __invoke(LoginRequest $request): StaffResource
-    {
-        // 認証
-        if (!Auth::guard('staffs')->attempt($request->only(['email', 'password']) + [
-            'status' => Staff::STATUS_ENABLED,
-            function($query) {
-                $query->whereHas('company', function($query) {
-                    $query->whereIn('status', [ManagementCompany::STATUS_ENABLED, ManagementCompany::STATUS_SUSPEND]);
-                });
-            },
-        ] + ['token' => $request->token])) {
-            throw new AuthenticationException();
-        }
+      // 他の認証はログアウト
+      Auth::guard('guests')->logout();
+      Auth::guard('brokers')->logout();
+      Auth::guard('administrators')->logout();
+      // セッションを再生成
+      $request->session()->regenerate();
 
-        // 他の認証はログアウト
-        Auth::guard('guests')->logout();
-        Auth::guard('brokers')->logout();
-        Auth::guard('administrators')->logout();
-        // セッションを再生成
-        $request->session()->regenerate();
+      $this->addOperationLog(OperationLog::OPERATION_TYPE_LOGIN, "管理会社社員ID", User::AuthStaff()->id);
 
-        $this->addOperationLog(OperationLog::OPERATION_TYPE_LOGIN, "管理会社社員ID", User::AuthStaff()->id);
-
-        return new StaffResource(User::AuthStaff());
-    }
+      return new StaffResource(User::AuthStaff());
+  }
 }
