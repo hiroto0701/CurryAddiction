836b4b856c191df7822abfb4ff6948f8f6281ab0
hiroto <hiroto.soccer.0701@gmail.com>
2024-05-06 23:29:38 +0900

    App.vue不要コード削除

--- frontend-user/src/App.vue
+++ frontend-user/src/App.vue
@@ -10,10 +10,4 @@ const commonStore = useCommonStore()
     v-if="commonStore.state.flashMessage"
     :message="commonStore.state.flashMessage"
   />
-  <!-- <div v-show="commonStore.state.apiLoading" class="flex justify-center my-20 fixed top-1/2 -translate-y-1/2 left-1/2">
-    <div class="animate-ping h-4 w-4 bg-sky-400 rounded-full"></div>
-  </div> -->
-  <!-- <div v-show="commonStore.state.apiLoading" class="flex justify-center my-20 fixed top-1/2 -translate-y-1/2 left-1/2">
-    <div class="animate-spin h-5 w-5 border-2 border-blue-400 rounded-full" style="border-top-color: transparent"></div>
-  </div> -->
 </template>
