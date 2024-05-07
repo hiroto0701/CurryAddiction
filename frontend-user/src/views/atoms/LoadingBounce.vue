27d2d7e369d3e4e98d53999a575fdf805d161492
hiroto <hiroto.soccer.0701@gmail.com>
2024-05-08 00:01:14 +0900

    微修正

--- frontend-user/src/views/atoms/LoadingBounce.vue
+++ frontend-user/src/views/atoms/LoadingBounce.vue
@@ -1,10 +1,7 @@
 <script setup lang="ts">
-import { useCommonStore } from '@/stores/common'
-
-const commonStore = useCommonStore()
 </script>
 <template>
-  <div v-show="commonStore.state.loginLoading" class="flex justify-center fixed top-1/3 left-1/2 -translate-x-1/2" aria-label="読み込み中">
+  <div class="flex justify-center fixed top-1/3 left-1/2 -translate-x-1/2" aria-label="読み込み中">
     <div class="animate-ping h-4 w-4 bg-sky-400 rounded-full"></div>
   </div>
 </template>
