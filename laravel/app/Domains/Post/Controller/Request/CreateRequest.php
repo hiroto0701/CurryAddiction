f894ba43de73584d83adaad5765ad20d6140c96e
hiroto <hiroto.soccer.0701@gmail.com>
2024-06-04 23:49:17 +0900

    10mbまでアップロードできるようにnginxの設定変更,投稿機能のpolicy,バリデーション等を変更

--- laravel/app/Domains/Post/Controller/Request/CreateRequest.php
+++ laravel/app/Domains/Post/Controller/Request/CreateRequest.php
@@ -33,46 +33,4 @@ class CreateRequest extends FormRequest
             'longitude' => ['required', 'numeric'],
         ];
     }
-
-    // public function withValidator(Validator $validator)
-    // {
-    //     $validator->sometimes(
-    //         'required_document_upload_file',
-    //         ['required', 'file', 'max:' . config('validation.maxFileSize'), new UploadableExtension()],
-    //         function ($input) {
-    //             return (int)$input->required_document_type !== IssuanceRequest::DOCUMENT_TYPE_HIDDEN
-    //             && (int)$input->required_document_type !== IssuanceRequest::DOCUMENT_TYPE_UNNECESSARY
-    //             && (int)$input->required_document_type !== IssuanceRequest::DOCUMENT_TYPE_ATTACHED_NONE;
-    //         }
-    //     );
-
-    //     $validator->after(function ($validator) {
-    //         // 物件に紐づく管理会社が現在利用可能か
-    //         $buildingId = $this->building_id;
-    //         $isEnableManagementCompany = ManagementCompany::whereHas('buildings', function ($query) use ($buildingId) {
-    //             $query->where('id', $buildingId);
-    //         })->where('status', ManagementCompany::STATUS_ENABLED)->exists();
-    //         if (!$isEnableManagementCompany) {
-    //             $validator->errors()->add(
-    //                 'building_id',
-    //                 __('validation.request_suspended', ['attribute' => $this->attributes()['building_id']])
-    //             );
-    //         }
-    //     });
-    // }
-
-    // public function attributes()
-    // {
-    //     return [
-    //         'building_id' => '依頼物件',
-    //         'building_house_number' => '依頼物件住戸番号',
-    //     ];
-    // }
-
-    // public function messages()
-    // {
-    //     return [
-    //         'required_document_upload_file.max' => __('validation.max_10mb'),
-    //     ];
-    // }
 }
