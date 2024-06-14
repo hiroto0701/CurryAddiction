3ce478e4637beb9f1d1460420a5b32274ea98875
inagaki <h.inagaki@hiyoku.co.jp>
2024-06-14 18:30:53 +0900

    paginationスタイル調整

---
+++ frontend-user/src/views/atoms/PageItem.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+interface Props {
+  text: string | number
+  active?: boolean
+}
+defineProps<Props>()
+</script>
+<template>
+  <li
+    class="relative inline-flex items-center px-4 py-2 font-body text-sumi-500 text-sm border border-gray-20 hover:bg-gray-50 focus:z-20 focus:outline-offset-0 first-of-type:rounded-l-full last-of-type:rounded-r-full cursor-pointer"
+    :class="{ 'bg-sumi-100 text-sumi-900': active }"
+  >
+    <button type="button">
+      {{ text }}
+    </button>
+  </li>
+</template>
--- frontend-user/src/views/molecules/Pagination.vue
+++ frontend-user/src/views/molecules/Pagination.vue
@@ -1,17 +1,108 @@
 <script setup lang="ts">
+import { computed } from 'vue'
 import { type PaginationStatus } from '@/stores/post_index'
+import PageItem from '@/views/atoms/PageItem.vue'
+
 interface Props {
   readonly paginationStatus: PaginationStatus
 }
-defineProps<Props>()
+const props = defineProps<Props>()
+
+const visiblePages = computed(() => {
+  const BOTH_SIDE_PAGE_COUNT = 2
+  const MAX_VISIBLE_PAGES = 7
+  const currentPage = props.paginationStatus.current_page || 1
+  const lastPage = props.paginationStatus.last_page || 1
+
+  if (lastPage <= MAX_VISIBLE_PAGES) {
+    return Array.from({ length: lastPage }, (_, i) => i + 1)
+  }
+
+  let pages = []
+  let startPage = Math.max(currentPage - BOTH_SIDE_PAGE_COUNT, 1)
+  let endPage = Math.min(currentPage + BOTH_SIDE_PAGE_COUNT, lastPage)
+
+  if (currentPage <= BOTH_SIDE_PAGE_COUNT + 1) {
+    endPage = Math.min(MAX_VISIBLE_PAGES - 2, lastPage - 2)
+  } else if (currentPage >= lastPage - BOTH_SIDE_PAGE_COUNT) {
+    startPage = Math.max(3, lastPage - (MAX_VISIBLE_PAGES - 3))
+  }
+
+  if (startPage > 2) {
+    pages.push(1)
+    pages.push('...')
+  } else {
+    startPage = 1
+  }
+
+  for (let i = startPage; i <= endPage; i++) {
+    pages.push(i)
+  }
+
+  if (endPage < lastPage - 1) {
+    pages.push('...')
+    pages.push(lastPage)
+  } else if (endPage === lastPage - 1) {
+    pages.push(lastPage)
+  }
+
+  return pages
+})
+
+const canPrev = computed((): boolean => {
+  const currentPage = props.paginationStatus.current_page || 1
+  return 1 < currentPage
+})
+
+const canNext = computed((): boolean => {
+  const currentPage = props.paginationStatus.current_page || 1
+  const lastPage = props.paginationStatus.last_page || 1
+  return currentPage < lastPage
+})
+
+const emit = defineEmits<{
+  (e: 'change-page', visiblePage: number | string): void
+}>()
+
+function doPrev(): void {
+  if (canPrev.value) {
+    const currentPage = props.paginationStatus.current_page || 1
+    emit('change-page', currentPage - 1)
+  }
+}
+
+function doNext(): void {
+  if (canNext.value) {
+    const currentPage = props.paginationStatus.current_page || 1
+    emit('change-page', currentPage + 1)
+  }
+}
 </script>
 <template>
   <div>
-    <p class="font-body text-sumi-900 text-sm">
-      全{{ paginationStatus.total }}件中 {{ paginationStatus.from }}件～{{
-        paginationStatus.to
+    <p class="font-body text-sumi-900 text-sm" v-if="paginationStatus.total !== null">
+      全{{ paginationStatus.total }}件中 {{ paginationStatus.from || 0 }}件～{{
+        paginationStatus.to || 0
       }}件を表示中
     </p>
-    <p class="font-body text-sumi-900 text-sm">{{ paginationStatus.current_page }}ページ目</p>
+    <p class="font-body text-sumi-900 text-sm" v-if="paginationStatus.current_page !== null">
+      {{ paginationStatus.current_page }}ページ目
+    </p>
+    <ul
+      v-if="visiblePages.length"
+      class="isolate flex -space-x-px rounded-md shadow-sm w-fit mx-auto"
+      aria-label="Pagination"
+    >
+      <PageItem text="前へ" @click="doPrev" :disabled="!canPrev" />
+      <PageItem
+        v-for="(visiblePage, key) in visiblePages"
+        :key="key"
+        :active="paginationStatus.current_page === visiblePage"
+        :text="visiblePage === '...' ? '...' : visiblePage"
+        @click="visiblePage !== '...' && emit('change-page', visiblePage)"
+        :disabled="visiblePage === '...'"
+      />
+      <PageItem text="次へ" @click="doNext" :disabled="!canNext" />
+    </ul>
   </div>
 </template>
