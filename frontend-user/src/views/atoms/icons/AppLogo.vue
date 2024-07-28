9af8e5daf07ab297052a4f1f7cff9a90ee82c2ed
hiroto inagaki <hiroto.soccer.0701@gmail.com>
2024-07-29 00:30:32 +0900

    fix: ロゴの追加およびS3で管理するように変更。

--- frontend-user/src/views/atoms/icons/AppLogo.vue
+++ frontend-user/src/views/atoms/icons/AppLogo.vue
@@ -3,6 +3,10 @@ import { RouterLink } from 'vue-router'
 </script>
 <template>
   <router-link :to="{ name: 'Home' }">
-    <img class="h-8 w-auto" src="../../../assets/logo.svg" alt="App Logo" />
+    <img
+      class="h-8 w-auto"
+      src="http://localhost:9000/local-curry_addiction/logo/app-logo.svg"
+      alt="App Logo"
+    />
   </router-link>
 </template>
