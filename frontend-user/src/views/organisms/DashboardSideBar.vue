e3f3b93562f0c170b754937885f8cd38fbe220ac
inagaki <h.inagaki@hiyoku.co.jp>
2024-07-30 15:24:07 +0900

    fix: ダッシュボードのサイドバーコンポーネントでtailwind-valiantsを使用するように修正

--- frontend-user/src/views/organisms/DashboardSideBar.vue
+++ frontend-user/src/views/organisms/DashboardSideBar.vue
@@ -2,6 +2,7 @@
 import { ref } from 'vue'
 import type { Component } from 'vue'
 import { useRouter } from 'vue-router'
+import { tv } from 'tailwind-variants'
 import DashboardIcon from '@/views/atoms/icons/DashboardIcon.vue'
 import HeartIcon from '@/views/atoms/icons/HeartIcon.vue'
 import ArchiveIcon from '@/views/atoms/icons/ArchiveIcon.vue'
@@ -54,27 +55,55 @@ const isMenuOpen = ref<boolean>(false)
 
 function openMenu(): void {
   isMenuOpen.value = true
-  document.body.style.overflow = 'hidden'
 }
 
 function closeMenu(): void {
   isMenuOpen.value = false
-  document.body.style.overflow = 'auto'
 }
+
+const menuButton = tv({
+  base: 'fixed right-3 top-8 flex h-12 w-12 items-center justify-center rounded-full border border-gray-300 bg-white opacity-70 shadow-sm duration-300 hover:bg-slate-100 hover:opacity-100',
+  variants: {
+    visibility: {
+      hidden: 'md:hidden'
+    }
+  },
+  defaultVariants: {
+    visibility: 'hidden'
+  }
+})
+
+const sidebar = tv({
+  base: 'fixed left-0 top-0 z-20 h-full transform overflow-y-auto bg-white p-5 transition-transform duration-300 ease-in-out md:sticky',
+  variants: {
+    isOpen: {
+      true: 'translate-x-0',
+      false: '-translate-x-full md:translate-x-0'
+    }
+  },
+  defaultVariants: {
+    isOpen: false
+  }
+})
+
+const overlay = tv({
+  base: 'fixed inset-0 z-10 bg-black bg-opacity-50',
+  variants: {
+    visibility: {
+      hidden: 'md:hidden'
+    }
+  },
+  defaultVariants: {
+    visibility: 'hidden'
+  }
+})
 </script>
 <template>
-  <button
-    class="fixed right-3 top-8 flex h-8 w-8 items-center justify-center rounded-lg border border-gray-300 bg-white opacity-70 shadow-sm duration-300 hover:bg-slate-100 hover:opacity-100 md:hidden"
-    aria-label="ダッシュボードメニューを開く"
-    @click="openMenu"
-  >
+  <button :class="menuButton()" aria-label="ダッシュボードメニューを開く" @click="openMenu">
     <MenuBarIcon class="text-sumi-900" />
   </button>
-  <aside
-    class="fixed left-0 top-0 z-20 h-full transform overflow-y-auto bg-white p-5 transition-transform duration-300 ease-in-out md:sticky md:translate-x-0"
-    :class="{ '-translate-x-full': !isMenuOpen }"
-    style="width: 258px"
-  >
+
+  <aside :class="sidebar({ isOpen: isMenuOpen })" style="width: 258px">
     <DashBoardSidebarItem
       :to="{ name: 'Home' }"
       label="ホームへ"
@@ -95,9 +124,6 @@ function closeMenu(): void {
       />
     </div>
   </aside>
-  <div
-    v-if="isMenuOpen"
-    class="fixed inset-0 z-10 bg-black bg-opacity-50 md:hidden"
-    @click="closeMenu"
-  ></div>
+
+  <div v-if="isMenuOpen" :class="overlay()" @click="closeMenu"></div>
 </template>
