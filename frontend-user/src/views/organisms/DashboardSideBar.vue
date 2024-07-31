d6ddbac49ab80cccae574393eec64e6c239fccfb
inagaki <h.inagaki@hiyoku.co.jp>
2024-07-31 09:46:59 +0900

    refactor: ハンバーガーメニューのhover時のエフェクトをPC画面以下では削除

--- frontend-user/src/views/organisms/DashboardSideBar.vue
+++ frontend-user/src/views/organisms/DashboardSideBar.vue
@@ -62,7 +62,7 @@ function closeMenu(): void {
 }
 
 const menuButton = tv({
-  base: 'fixed right-3 top-8 flex h-12 w-12 items-center justify-center rounded-full border border-gray-300 bg-white opacity-70 shadow-sm duration-300 hover:bg-slate-100 hover:opacity-100',
+  base: 'fixed right-3 top-8 flex h-12 w-12 items-center justify-center rounded-full border border-gray-300 bg-white opacity-70 shadow-sm duration-300',
   variants: {
     visibility: {
       hidden: 'md:hidden'
@@ -70,7 +70,13 @@ const menuButton = tv({
   },
   defaultVariants: {
     visibility: 'hidden'
-  }
+  },
+  compoundVariants: [
+    {
+      visibility: 'hidden',
+      class: 'md:hover:bg-slate-100 md:hover:opacity-100'
+    }
+  ]
 })
 
 const sidebar = tv({
