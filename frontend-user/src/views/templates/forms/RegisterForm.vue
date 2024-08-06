df7a086e5dbf92772f1ac2d916e747ef2ec8ee7a
inagaki <h.inagaki@hiyoku.co.jp>
2024-08-06 13:05:24 +0900

    fix: 投稿作成フォームのUI修正

--- frontend-user/src/views/templates/forms/RegisterForm.vue
+++ frontend-user/src/views/templates/forms/RegisterForm.vue
@@ -1,16 +1,8 @@
 <script setup lang="ts">
 import BaseForm from '@/views/templates/forms/BaseForm.vue'
-
-interface Props {
-  readonly title: string
-}
-defineProps<Props>()
 </script>
 <template>
-  <BaseForm :title="title">
+  <BaseForm>
     <slot />
-    <!-- <template #submit-button>
-      <register-button class="px-5" />
-    </template> -->
   </BaseForm>
 </template>
