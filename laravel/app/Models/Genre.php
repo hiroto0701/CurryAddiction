ef2b62fc1c01001f704923f3e12249f1079ec6d8
hiroto inagaki <hiroto.soccer.0701@gmail.com>
2024-08-12 21:29:22 +0900

    fix: カレージャンルモデル、マイグレーションファイル修正

--- laravel/app/Models/Genre.php
+++ laravel/app/Models/Genre.php
@@ -4,8 +4,23 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Traits\OperatorRecordable;
 
 class Genre extends Model
 {
     use HasFactory;
+    use OperatorRecordable;
+
+    protected $table = 'likes';
+
+    protected $fillable = [
+        'name',
+    ];
+
+    protected $dates = [
+        'uploaded_at',
+        'created_at',
+        'updated_at',
+        'deleted_at',
+    ];
 }
