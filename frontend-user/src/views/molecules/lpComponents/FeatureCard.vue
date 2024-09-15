a1d3307e6be7e399ac711e0e224aeef35178f09c
hiroto inagaki <hiroto.soccer.0701@gmail.com>
2024-09-15 17:30:30 +0900

    fix: LPの機能紹介アイコンの色変更

--- frontend-user/src/views/molecules/lpComponents/FeatureCard.vue
+++ frontend-user/src/views/molecules/lpComponents/FeatureCard.vue
@@ -11,7 +11,7 @@ defineProps<Props>();
 <template>
   <div class="border-sumi-150 rounded-xl border p-7">
     <div class="flex items-center justify-center">
-      <component :is="icon" class="w-7" />
+      <component :is="icon" class="w-7 text-sumi-800" />
     </div>
     <h3 class="mt-1 flex items-center justify-center font-body text-lg font-medium text-sumi-900">
       {{ title }}
