00ffb4418b85a379acadb0c73bf06532de701fcf
inagaki <h.inagaki@hiyoku.co.jp>
2024-10-01 14:54:01 +0900

    add: 絞り込み条件があったらアイコンにインジケータを付与するように実装

--- frontend-user/src/views/molecules/buttons/PostFilterButton.vue
+++ frontend-user/src/views/molecules/buttons/PostFilterButton.vue
@@ -1,16 +1,24 @@
 <script setup lang="ts">
+import { useAccountStore } from '@/stores/account';
 import FilterIcon from '@/views/atoms/icons/FilterIcon.vue';
+
+const accountStore = useAccountStore();
 
 const emit = defineEmits<{
   (e: 'toggleMenu'): void;
 }>();
 </script>
+
 <template>
   <button
-    class="group absolute -top-12 right-0 flex aspect-square w-9 items-center justify-center rounded-full transition-opacity duration-500 hover:bg-gray-100"
+    class="group relative mb-5 ml-auto mr-0 flex aspect-square w-9 items-center justify-center rounded-full transition-opacity duration-500 hover:bg-gray-100"
     aria-label="メニューを開く"
     @click="emit('toggleMenu')"
   >
-    <FilterIcon class="w-6 text-sumi-600 group-hover:text-sumi-900" />
+    <FilterIcon class="relative w-6 text-sumi-600 group-hover:text-sumi-900" />
+    <div
+      class="absolute -right-[1px] -top-[1px] h-2 w-2 rounded-full bg-red-400"
+      v-if="accountStore.state.favorite_genres.length > 0"
+    ></div>
   </button>
 </template>
