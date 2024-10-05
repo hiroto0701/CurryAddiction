00790e75a06cd8f0e9198ff216bcf2200ace9408
hiroto inagaki <hiroto.soccer.0701@gmail.com>
2024-10-05 23:25:19 +0900

    add: 通知がない場合の表示を追加

--- frontend-user/src/views/molecules/NotificationList.vue
+++ frontend-user/src/views/molecules/NotificationList.vue
@@ -3,6 +3,7 @@ import { MenuItems } from '@headlessui/vue';
 import type { Notification, PaginationStatus } from '@/types/notification';
 import NotificationItem from '@/views/molecules/NotificationItem.vue';
 import NotificationLoadButton from '@/views/molecules/buttons/NotificationLoadButton.vue';
+import EmptyNotification from '@/views/molecules/EmptyNotification.vue';
 
 interface Props {
   readonly notificationData: Notification[];
@@ -19,7 +20,7 @@ const emit = defineEmits<{
   <MenuItems
     class="flex w-72 flex-col items-center justify-center rounded bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5"
   >
-    <div class="max-h-80 overflow-y-auto">
+    <div class="max-h-80 overflow-y-auto" v-if="notificationData.length > 0">
       <NotificationItem
         v-for="notification in notificationData"
         :key="notification.id"
@@ -33,5 +34,6 @@ const emit = defineEmits<{
         />
       </div>
     </div>
+    <EmptyNotification v-else />
   </MenuItems>
 </template>
