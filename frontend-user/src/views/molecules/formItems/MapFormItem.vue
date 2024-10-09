96ce1fa0ebb9f388c3b0ca90c3557947ea8855cc
inagaki <h.inagaki@hiyoku.co.jp>
2024-10-09 15:11:08 +0900

    add: GoogleMap導入、autocomplete実装

--- frontend-user/src/views/molecules/formItems/MapFormItem.vue
+++ frontend-user/src/views/molecules/formItems/MapFormItem.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import { ref, onMounted } from 'vue';
 import type { Component } from 'vue';
 import FormLayout from '@/views/templates/FormLayout.vue';
 
@@ -8,14 +9,88 @@ interface Props {
   readonly iconComponent: Component;
 }
 defineProps<Props>();
+
+const mapContainer = ref<HTMLElement | null>(null);
+const map = ref<google.maps.Map | null>(null);
+const searchBox = ref<google.maps.places.SearchBox | null>(null);
+
+function initMap(position?: GeolocationPosition) {
+  if (mapContainer.value) {
+    const defaultPosition = { lat: 35.6812, lng: 139.7671 }; // 東京駅をデフォルト位置にする
+    const center = position
+      ? { lat: position.coords.latitude, lng: position.coords.longitude }
+      : defaultPosition;
+
+    // 地図の初期化
+    map.value = new google.maps.Map(mapContainer.value, {
+      center: center,
+      zoom: 15
+    });
+
+    // 検索ボックス初期化
+    const input = document.createElement('input');
+    input.setAttribute('type', 'text');
+    input.setAttribute('placeholder', '場所を検索');
+    map.value.controls[google.maps.ControlPosition.TOP_RIGHT].push(input);
+
+    searchBox.value = new google.maps.places.SearchBox(input);
+
+    // 検索結果にバイアスをかける
+    map.value.addListener('bounds_changed', () => {
+      searchBox.value?.setBounds(map.value?.getBounds() as google.maps.LatLngBounds);
+    });
+
+    searchBox.value.addListener('places_changed', () => {
+      const places = searchBox.value?.getPlaces();
+      if (places?.length === 0) {
+        return;
+      }
+
+      const bounds = new google.maps.LatLngBounds();
+      places?.forEach((place) => {
+        if (!place.geometry || !place.geometry.location) {
+          console.log('Returned place contains no geometry');
+          return;
+        }
+
+        // ピンの作成
+        new google.maps.Marker({
+          map: map.value,
+          title: place.name,
+          position: place.geometry.location
+        });
+
+        if (place.geometry.viewport) {
+          bounds.union(place.geometry.viewport);
+        } else {
+          bounds.extend(place.geometry.location);
+        }
+      });
+      map.value?.fitBounds(bounds);
+    });
+  }
+}
+
+onMounted(() => {
+  if ('geolocation' in navigator) {
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        initMap(position);
+      },
+      (error) => {
+        console.error('Error getting current location:', error);
+        initMap();
+      },
+      { enableHighAccuracy: true, timeout: 5000, maximumAge: 0 }
+    );
+  } else {
+    console.log('Geolocation is not supported by this browser.');
+    initMap();
+  }
+});
 </script>
 <template>
-  <FormLayout :label :required :iconComponent>
-    <iframe
-      src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3323.873985289111!2d130.37126177569635!3d33.58262097333749!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x354193c915f0ee39%3A0xb1db154a340d16ee!2z44Ki44OV44K_44O844Kw44Ot44Km!5e0!3m2!1sja!2sjp!4v1710258613983!5m2!1sja!2sjp"
-      class="h-96 w-full"
-      loading="lazy"
-      referrerpolicy="no-referrer-when-downgrade"
-    ></iframe>
+  <FormLayout :label="label" :required="required" :iconComponent="iconComponent">
+    <div ref="mapContainer" style="width: 100%; height: 400px"></div>
   </FormLayout>
 </template>
