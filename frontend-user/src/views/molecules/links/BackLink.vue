456702365f75232728649d882d95c9e6b9e42b43
inagaki <h.inagaki@hiyoku.co.jp>
2024-10-15 09:33:23 +0900

    fix: 投稿作成コンポーネントのバックリンク先修正

--- frontend-user/src/views/molecules/links/BackLink.vue
+++ frontend-user/src/views/molecules/links/BackLink.vue
@@ -1,16 +1,19 @@
 <script setup lang="ts">
 import { ref, computed } from 'vue';
-import { RouterLink } from 'vue-router';
+import { RouterLink, useRoute } from 'vue-router';
 import type { RouteLocationNormalized, RouteLocationRaw } from 'vue-router';
 import { useCommonStore } from '@/stores/common';
 import BackButtonIcon from '@/views/atoms/icons/BackButtonIcon.vue';
 import BottomTooltip from '@/views/molecules/tooltips/BottomTooltip.vue';
 
 const commonStore = useCommonStore();
+const route = useRoute();
 const referrer = ref<RouteLocationNormalized | null>(commonStore.state.originalRoute);
 
 const linkDestination = computed<RouteLocationRaw>(() => {
-  if (referrer.value) {
+  if (route.name === 'PostCreate' || !referrer.value) {
+    return { name: 'Home' };
+  } else {
     const { name, path, query, params } = referrer.value;
     return {
       name,
@@ -18,13 +21,15 @@ const linkDestination = computed<RouteLocationRaw>(() => {
       query: { ...query },
       params: { ...params }
     };
-  } else {
-    return { name: 'Home' };
   }
+});
+
+const tooltipText = computed(() => {
+  return route.name === 'PostCreate' || !referrer.value ? 'ホームへ戻る' : '戻る';
 });
 </script>
 <template>
-  <BottomTooltip text="戻る" position="bottom">
+  <BottomTooltip :text="tooltipText" position="bottom">
     <router-link
       :to="linkDestination"
       class="peer flex aspect-square w-8 items-center justify-center rounded-full transition-opacity duration-500 hover:bg-gray-100"
