6ed3fd51f0e9dbf7a47bb2081cfd4b578006b9a8
hiroto inagaki <hiroto.soccer.0701@gmail.com>
2024-10-22 15:47:50 +0900

    add: firebase-hostingにデプロイ設定追加

--- frontend-user/src/main.ts
+++ frontend-user/src/main.ts
@@ -10,6 +10,22 @@ import type { Dayjs } from 'dayjs';
 import ja from 'dayjs/locale/ja';
 import utc from 'dayjs/plugin/utc';
 import timezone from 'dayjs/plugin/timezone';
+import { initializeApp } from 'firebase/app';
+import { getAnalytics } from 'firebase/analytics';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyCGBDpw868I0RuRL90NJ7gy1L3NISpalow',
+  authDomain: 'curryaddiction-8772e.firebaseapp.com',
+  projectId: 'curryaddiction-8772e',
+  storageBucket: 'curryaddiction-8772e.appspot.com',
+  messagingSenderId: '964769376449',
+  appId: '1:964769376449:web:703c92df84d25f10b44c40',
+  measurementId: 'G-EQD1QTC5GV'
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 axios.defaults.baseURL = import.meta.env.VITE_APP_API_ENDPOINT;
 axios.defaults.withCredentials = true;
