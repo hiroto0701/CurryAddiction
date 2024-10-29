523568e5c904faa8d08787fc08bbc9df4b2824be
inagaki <h.inagaki@hiyoku.co.jp>
2024-10-29 13:23:09 +0900

    fix: 不要ファイル削除に伴うコードの修正

--- frontend-admin/src/main.ts
+++ frontend-admin/src/main.ts
@@ -1,18 +1,36 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-
 import App from './App.vue'
 import router from './router'
-
 import './style.css'
 import axios from 'axios'
+import dayjs from 'dayjs'
+import type { Dayjs } from 'dayjs'
+import ja from 'dayjs/locale/ja'
+import utc from 'dayjs/plugin/utc'
+import timezone from 'dayjs/plugin/timezone'
 
-// api側にリクエスト送る際のURLを設定
-axios.defaults.baseURL = import.meta.env.VITE_APP_API_ENDPOINT;
+axios.defaults.baseURL = import.meta.env.VITE_APP_API_ENDPOINT
+axios.defaults.withCredentials = true
+axios.defaults.withXSRFToken = true
+axios.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if ([403, 404].includes(error.response.status)) {
+      // 一律404画面を表示する
+      router.replace({ name: 'ErrorNotFound' })
+    }
+    return Promise.reject(error)
+  }
+)
 
-const app = createApp(App)
+dayjs.extend(utc)
+dayjs.extend(timezone)
+dayjs.locale(ja)
+dayjs.tz.setDefault('Asia/Tokyo')
 
-app.use(createPinia())
-app.use(router)
-
-app.mount('#app')
+createApp(App)
+  .use(createPinia())
+  .use(router)
+  .provide('$dayjs', dayjs as (date?: string | number | Date | Dayjs) => Dayjs)
+  .mount('#app')
